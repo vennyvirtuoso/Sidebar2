3d393afe18f192dfec8e2e816ef9f5d9d0113bc7
vennyvirtuoso <vijendra9753@gmail.com>
2023-03-27 17:10:31 +0530

    Initial Commit

---
+++ Sidebar/SidebarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SidebarApp.swift
+//  Sidebar
+//
+//  Created by Vijendra Vaishya on 27/03/23.
+//
+
+import SwiftUI
+
+@main
+struct SidebarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
